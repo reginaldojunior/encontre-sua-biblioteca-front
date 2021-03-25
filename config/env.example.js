92cb5e35a50a6a2ce6da0ca5eaaff24ca3da086d
Reginaldo Junior <reginaldo.junior@vagas.com.br>
2021-03-25 11:15:38 -0300

    Adiciona o arquivo .env

--- config/env.example.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    KEY_GMAPS: "",
-    URL_BACKEND: "http://localhost:3000"
-}
---
+++ config/env.js
@@ -0,0 +1,4 @@
+module.exports = {
+    KEY_GMAPS: process.env.VUE_APP_KEY_GMAPS,
+    URL_BACKEND: process.env.VUE_APP_URL_BACKEND
+}
